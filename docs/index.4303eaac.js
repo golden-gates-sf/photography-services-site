b3b56ef91894aa854874295dc1c0a9d0ea31da25
golden-gates-sf <redchenko.milana@gmail.com>
2024-01-25 22:15:28 +0300

    docs: create docs directory

---
+++ docs/index.4303eaac.js
@@ -0,0 +1,6 @@
+const tempModal = document.querySelector(".temp-modal-window");
+tempModal.addEventListener("click", ()=>{
+    tempModal.remove();
+});
+
+//# sourceMappingURL=index.4303eaac.js.map
