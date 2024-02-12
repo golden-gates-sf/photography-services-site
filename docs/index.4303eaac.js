440ba8028e0d33378b132e0650aee8fda6202dad
golden-gates-sf <redchenko.milana@gmail.com>
2024-02-12 15:00:38 +0300

    feat: add deployment from dist

--- docs/index.4303eaac.js
+++
@@ -1,6 +0,0 @@
-const tempModal = document.querySelector(".temp-modal-window");
-tempModal.addEventListener("click", ()=>{
-    tempModal.remove();
-});
-
-//# sourceMappingURL=index.4303eaac.js.map
--- docs/index.def99abe.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".temp-modal-window");e.addEventListener("click",()=>{e.remove()});
-//# sourceMappingURL=index.def99abe.js.map
